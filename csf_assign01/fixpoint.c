39118586c78434af92a980b5fc97e04a78011c22
Caroline Jia <carolinejia@carolines-air-3.acasmart.jh.edu>
2025-08-28 20:07:39 -0400

    first few functions done

--- csf_assign01/fixpoint.c
+++ csf_assign01/fixpoint.c
@@ -17,32 +17,44 @@
 
 void
 fixpoint_init( fixpoint_t *val, uint32_t whole, uint32_t frac, bool negative ) {
-  // TODO: implement
+  val->whole = whole;
+  val->frac = frac;
+  val->negative = negative;
+
 }
 
 uint32_t
 fixpoint_get_whole( const fixpoint_t *val ) {
-  // TODO: implement
+  return val->whole;
 }
 
 uint32_t
 fixpoint_get_frac( const fixpoint_t *val ) {
-  // TODO: implement
+  return val->frac;
 }
 
 bool
 fixpoint_is_negative( const fixpoint_t *val ) {
-  // TODO: implement
+  return val->negative;
 }
 
 void
 fixpoint_negate( fixpoint_t *val ) {
-  // TODO: implement
+  if(val->frac != 0 || val->whole != 0){
+    val->negative = !(val->negative);
+  }
+  else {
+    val->negative = 0;
+  }
 }
 
 result_t
 fixpoint_add( fixpoint_t *result, const fixpoint_t *left, const fixpoint_t *right ) {
-  // TODO: implement
+  //check if signs are same
+  //if same then just add all the values and keep same sign
+  //if different then find bigger whole and subtract smaller from bigger number
+  //if wholes are both zero then do that with the fractions
+  //
 }
 
 result_t
